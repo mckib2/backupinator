c12edef9d4685761976c9ff8b73307e865059662
mckib2 <nicholas.bgp@gmail.com>
2019-12-13 15:44:11 -0700

    latest changes

--- backupinator/target.py
+++ backupinator/target.py
@@ -3,9 +3,15 @@
 import logging
 
 class Target:
+    '''Runs at remote site and gets client data from server.'''
 
-    def __init__(self):
+    def __init__(self, target_name):
+
+        self.target_name = target_name
 
         # Set debug level
         log_format = "%(levelname)s:[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
         logging.basicConfig(format=log_format, level=logging.DEBUG)
+
+        # # Get target name from config
+        # self.target_name = get_config_val('target_name')
