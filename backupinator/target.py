1cd305b8a0cbcc79b290b59eeb56431f5f641ada
nicholas <nicholas.bgp@gmail.com>
2019-12-13 18:30:26 -0700

    start thinking about how target will wirk'

--- backupinator/target.py
+++ backupinator/target.py
@@ -2,8 +2,30 @@
 
 import logging
 
+from backupinator import TargetDB
+
 class Target:
-    '''Runs at remote site and gets client data from server.'''
+    '''Runs at remote site and gets client data from server.
+
+    Notes
+    -----
+
+    Database:
+        Client Table:
+            clientID
+            last_active
+
+        Chunk Table:
+            hash(plaintext)
+            chunk_filename
+            chunk_owner
+
+        Filenames Table:
+            filename_owner
+            filename
+            version
+            timestamp
+    '''
 
     def __init__(self, target_name):
 
@@ -15,3 +37,6 @@ class Target:
 
         # # Get target name from config
         # self.target_name = get_config_val('target_name')
+
+        # get a database
+        self.target_db = TargetDB(self.target_name)
